db91c198689b4f3f90d6a15089923fba71276367
Adrian B <abrezai@gmail.com>
2012-11-12 13:15:37 -0800

    test

---
+++ js/MainMenu.js
@@ -0,0 +1,4 @@
+function MainMenuViewModel() {
+    var self = this; 
+    self.menus = ['Create New File', 'Menu 2', 'Menu 3', 'Menu4'];
+};
--- js/loadGrid.js
+++ js/loadGrid.js
@@ -1,37 +1,32 @@
 var initialData = [
-    { name: "Well-Travelled Kitten", sales: 352, price: 75.95 },
-    { name: "Speedy Coyote", sales: 89, price: 190.00 },
-    { name: "Furious Lizard", sales: 152, price: 25.00 },
-    { name: "Indifferent Monkey", sales: 1, price: 99.95 },
-    { name: "Brooding Dragon", sales: 0, price: 6350 },
-    { name: "Ingenious Tadpole", sales: 39450, price: 0.35 },
-    { name: "Optimistic Snail", sales: 420, price: 1.50 }
+    { name: "Sale_123456", type: "Sale", price: 75.95 },
+    { name: "HE_12345", type: "Home Equity", price: 190.00 },
+    { name: "Purchase_333", type: "Purchase", price: 25.00 },
+    { name: "MO_87877", type: "Mortgage Only", price: 99.95 },
+    { name: "Sale_3333", type: "Sale", price: 6350 },
+    { name: "Sale_3232", type: "Sale", price: 0.35 },
+    { name: "Ho_333", type: "Home Equity", price: 1.50 }
 ];
  
 var PagedGridModel = function(items) {
     this.items = ko.observableArray(items);
- 
-    this.addItem = function() {
-        this.items.push({ name: "New item", sales: 0, price: 100 });
-    };
- 
-    this.sortByName = function() {
-        this.items.sort(function(a, b) {
-            return a.name < b.name ? -1 : 1;
-        });
-    };
- 
-    this.jumpToFirstPage = function() {
-        this.gridViewModel.currentPageIndex(0);
-    };
+
  
     this.gridViewModel = new ko.simpleGrid.viewModel({
         data: this.items,
         columns: [
-            { headerText: "Item Name", rowText: "name" },
-            { headerText: "Sales Count", rowText: "sales" },
+            { headerText: "File Matter", rowText: "name" },
+            { headerText: "File Matter Type", rowText: "type" },
             { headerText: "Price", rowText: function (item) { return "$" + item.price.toFixed(2) } }
         ],
-        pageSize: 4
+        pageSize: 20
     });
+    
+    this.menus = ['Create New File', 'Efs', 'Find', 'News', 'Logout'];
+    
+    this.openForm=function(menu){
+      if(menu == "News")
+        $("#newsbox").overlay().load();
+    }
 };
+
